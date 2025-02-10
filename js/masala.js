d339b4da25927dd1a5c97b2166c92dd8f3a959be
Sening isming <Emailing>
2025-02-10 17:23:10 +0500

    masala ga yangi html va js

---
+++ js/masala.js
@@ -0,0 +1,11 @@
+// const user = {
+//     id:1,
+//     name:"Sardorbek",
+//     settings: {
+//         theme: "dark",
+//         notifications: null,
+//     },
+// }
+// console.log(user?.settings?.theme);
+// console.log(user?.settings?.notifications?.email);
+// console.log(user?.profile?.age ?? "Ma'lumot yo'q");
