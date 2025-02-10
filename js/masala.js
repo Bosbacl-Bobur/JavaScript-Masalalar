6c74455d890c064828049b5b68aae8ce2268d251
Sening isming <Emailing>
2025-02-10 17:27:40 +0500

    new Set()

--- js/masala.js
+++ js/masala.js
@@ -9,3 +9,14 @@
 // console.log(user?.settings?.theme);
 // console.log(user?.settings?.notifications?.email);
 // console.log(user?.profile?.age ?? "Ma'lumot yo'q");
+
+const raqam = new Set();
+
+raqam.add(1);
+raqam.add(2);
+raqam.add(3);
+raqam.add(4);
+raqam.add(5);
+raqam.add("hello");
+raqam.add(1n);
+console.log(raqam);
